470bdf0734027e42798b94ca33b03987242eceb6
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-06-15 08:38:36 +0530

    Inventory scanner app

---
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/entity/InventoryTransactionType.java
@@ -0,0 +1,16 @@
+package com.debasish.scanstockiq.entity;
+
+public enum InventoryTransactionType {
+
+    STOCK_IN,
+
+    SALE,
+
+    RETURN,
+
+    DAMAGE,
+
+    EXPIRED,
+
+    MANUAL_ADJUSTMENT
+}
