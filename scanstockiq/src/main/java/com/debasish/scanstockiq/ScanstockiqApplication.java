9ad71ab06f33e64819c8b6ea38763f64ac5794ea
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-06-15 08:25:11 +0530

    Scan stock project initial commit.

---
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/ScanstockiqApplication.java
@@ -0,0 +1,13 @@
+package com.debasish.scanstockiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanstockiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanstockiqApplication.class, args);
+	}
+
+}
---
+++ scanstockiq/src/test/java/com/debasish/scanstockiq/ScanstockiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debasish.scanstockiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanstockiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
