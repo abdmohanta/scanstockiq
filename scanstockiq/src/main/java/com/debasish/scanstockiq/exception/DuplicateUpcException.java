81b5d03290efb7635e6ced64a2ff79cff8d569a0
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-06-17 22:16:03 +0530

    Code changes for scanstockiq application

---
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/exception/DuplicateUpcException.java
@@ -0,0 +1,8 @@
+package com.debasish.scanstockiq.exception;
+
+public class DuplicateUpcException extends RuntimeException {
+
+    public DuplicateUpcException(String message) {
+        super(message);
+    }
+}
