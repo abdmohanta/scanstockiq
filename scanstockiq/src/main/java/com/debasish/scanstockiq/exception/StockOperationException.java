c325e785e244b963fdc62092221df87cb5d536a1
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-06-17 22:18:07 +0530

    Code changes for scanstockiq application

---
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/exception/StockOperationException.java
@@ -0,0 +1,8 @@
+package com.debasish.scanstockiq.exception;
+
+public class StockOperationException extends RuntimeException {
+
+    public StockOperationException(String message) {
+        super(message);
+    }
+}
