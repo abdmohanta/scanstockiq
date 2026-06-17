e10130cf40423ecdb8abbf450dbf9f5dc841c072
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-06-17 22:23:34 +0530

    Code changes for scanstockiq application

--- scanstockiq/src/main/java/com/debasish/scanstockiq/exception/InsufficientStockException.java
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/exception/InsufficientStockException.java
@@ -1,4 +1,7 @@
 package com.debasish.scanstockiq.exception;
 
 public class InsufficientStockException {
+    public String getMessage() {
+        return "Insufficient stock for the requested operation.";
+    }
 }
