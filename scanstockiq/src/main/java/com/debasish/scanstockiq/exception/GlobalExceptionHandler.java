15f85e8ebf0bd412a2751796766fe8a95f31e6b7
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-06-17 22:19:35 +0530

    Code changes for scanstockiq application

---
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.debasish.scanstockiq.exception;
+
+import com.debasish.scanstockiq.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleProductNotFound(
+            ProductNotFoundException ex) {
+
+        ErrorResponse response =
+                ErrorResponse.builder()
+                        .success(false)
+                        .errorCode("PRODUCT_NOT_FOUND")
+                        .message(ex.getMessage())
+                        .timestamp(LocalDateTime.now())
+                        .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
+}
