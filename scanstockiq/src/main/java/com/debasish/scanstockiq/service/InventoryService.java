0aa35552eb44fcb99df9d7964d211cad0565329d
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-06-26 07:44:01 +0530

    Code changes for scanstockiq application

--- scanstockiq/src/main/java/com/debasish/scanstockiq/service/InventoryService.java
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/service/InventoryService.java
@@ -1,7 +1,10 @@
 package com.debasish.scanstockiq.service;
 
+import com.debasish.scanstockiq.dto.InventoryHistoryResponse;
 import com.debasish.scanstockiq.dto.ReturnRequest;
 import com.debasish.scanstockiq.dto.StockOperationRequest;
+
+import java.util.List;
 
 public interface InventoryService {
 
@@ -14,4 +17,6 @@ public interface InventoryService {
     void manualAdjustment(StockOperationRequest request);
 
     void processReturn(ReturnRequest request);
+
+    List<InventoryHistoryResponse> getInventoryHistory(String upcCode);
 }
