472c912c8a60e75f2ca6844a5da8cc2649cad866
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-06-27 15:26:20 +0530

    Code changes for scanstockiq application

---
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/repository/SupplierRepository.java
@@ -0,0 +1,13 @@
+package com.debasish.scanstockiq.repository;
+
+import com.debasish.scanstockiq.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SupplierRepository
+        extends JpaRepository<Supplier, Long> {
+
+    Optional<Supplier> findBySupplierCode(String supplierCode);
+
+}
