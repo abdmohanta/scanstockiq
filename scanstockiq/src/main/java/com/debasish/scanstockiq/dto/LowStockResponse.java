5041baf693d05bd8f30e24ed70fd2b3a414d1d55
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-07-01 21:49:12 +0530

    Code changes for scanstockiq application

---
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/dto/LowStockResponse.java
@@ -0,0 +1,19 @@
+package com.debasish.scanstockiq.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class LowStockResponse {
+
+    private Long productId;
+
+    private String productName;
+
+    private String upcCode;
+
+    private Integer currentStock;
+
+    private Integer reorderLevel;
+}
