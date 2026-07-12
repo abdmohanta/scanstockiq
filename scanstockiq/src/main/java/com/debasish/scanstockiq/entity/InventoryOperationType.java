dd4bdabc6c94040012791472ac019808408b94e1
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-07-12 20:00:37 +0530

    Code changes for scanstockiq application

---
+++ scanstockiq/src/main/java/com/debasish/scanstockiq/entity/InventoryOperationType.java
@@ -0,0 +1,24 @@
+package com.debasish.scanstockiq.entity;
+
+public enum InventoryOperationType {
+
+    STOCK_IN,
+
+    SALE,
+
+    RETURN,
+
+    DAMAGE,
+
+    EXPIRED,
+
+    MANUAL_ADJUSTMENT,
+
+    PURCHASE_RECEIVED,
+
+    PURCHASE_CANCELLED,
+
+    STOCK_TRANSFER_IN,
+
+    STOCK_TRANSFER_OUT
+}
